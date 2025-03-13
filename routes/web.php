06884a8401b3c3e47a89c4160b55de15607a4d9e
ahlamelrhazi <ahlamelrhazi8@gmail.com>
2025-03-13 18:01:12 +0000

    Sauvegarde des modifications dans routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
 
 Route::resource('mode_reglements', ModeReglementController::class);
